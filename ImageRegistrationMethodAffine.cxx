4d278244c52d6e287dcb2f25808fb86827bfa782
Rozemarijn Weijers <r.a.m.weijers@student.tue.nl>
2015-11-19 17:42:55 -0500

    with volume slicer

--- ImageRegistrationMethodAffine.cxx
+++ ImageRegistrationMethodAffine.cxx
@@ -15,6 +15,11 @@
 #include "itkTranslationTransform.h"
 #include <itkMatrix.h>
 #include <itkNrrdImageIO.h>
+#include <itkExtractImageFilter.h>
+
+#include <vtkNrrdReader.h>
+#include "vtkSmartPointer.h"
+#include "vtkImageData.h"
 
 #include "igtlOSUtil.h"
 #include "igtlMessageHeader.h"
@@ -46,14 +51,22 @@ typedef itk::ImageRegistrationMethod< ImageType, ImageType >    RegistrationType
 
 class Images
 {
-   public:
+  public:
 
-   Images();
-   int IGTtoITKImage();
-   int ITKtoIGTImage();
+  Images();
+  int IGTtoITKImage();
+  int ITKtoIGTImage();
+  int SetParametersFromIGT();
+  int SetParametersFromITK();
 
-   ImageType::Pointer imageData;
-   igtl::ImageMessage::Pointer imgMsg;
+  ImageType::Pointer imageData;
+  igtl::ImageMessage::Pointer imgMsg;
+
+  protected:
+
+  int sizeIm[3];
+  float originIm[3];
+  float spacingIm[3];
 
 };
 
@@ -63,6 +76,31 @@ Images::Images()
   // Create imageMessage and ITKimage for this image
   imageData  = ImageType::New();
   imgMsg = igtl::ImageMessage::New();
+
+}
+
+int Images::SetParametersFromIGT()
+{
+
+  this->imgMsg->GetDimensions(this->sizeIm);
+  this->imgMsg->GetOrigin(this->originIm);
+  this->imgMsg->GetSpacing(this->spacingIm);
+
+  return 1;
+
+}
+
+int Images::SetParametersFromITK()
+{
+
+  ImageType::SizeType size = this->imageData->GetLargestPossibleRegion().GetSize();
+  sizeIm[0] = size[0]; sizeIm[1] = size[1]; sizeIm[2] = 1;
+  ImageType::PointType origin = this->imageData->GetOrigin();
+  originIm[0] = origin[0]; originIm[1] = origin[1]; originIm[2] = origin[2];
+  ImageType::SpacingType spacing = this->imageData->GetSpacing();
+  spacingIm[0] = spacing[0]; spacingIm[1] = spacing[1]; spacingIm[2] = spacing[2];
+
+  return 1;
 
 }
 
@@ -125,7 +163,7 @@ int Images::ITKtoIGTImage()
   start = this->imageData->GetLargestPossibleRegion().GetIndex();
   svoffset[0] = start[0]; svoffset[1] = start[1]; svoffset[2] = start[2];
   spacing = this->imageData->GetSpacing();
-  spacingIGT[0] = spacing[0]; spacingIGT[1] = spacing[1]; spacingIGT[2] = spacing[2];
+  spacingIGT[0] = spacing[0]; spacingIGT[1] = spacing[1]; spacingIGT[2] = spacing[1];//spacing[2];!!!!!!!!!!!
   scalarType = igtl::ImageMessage::TYPE_UINT8;
 
   // Create a new IMAGE type message
@@ -137,6 +175,77 @@ int Images::ITKtoIGTImage()
 
   // Set copy image data into ITK image
   memcpy(this->imgMsg->GetScalarPointer(), imageData->GetBufferPointer(), this->imgMsg->GetSubVolumeImageSize());
+  // Pack (serialize) and send
+  this->imgMsg->Pack();
+
+  return 1;
+
+}
+
+class Volumes
+{
+
+  public:
+
+  Volumes();
+  int ITKtoIGTVolume();
+
+  VolumeType::Pointer volumeData;
+  igtl::ImageMessage::Pointer imgMsg;
+
+  protected:
+
+  int sizeVol[3];
+  float originVol[3];
+  float spacingVol[3];
+
+};
+
+Volumes::Volumes()
+{
+
+  // Create imageMessage and ITKvolume for this volume
+  volumeData  = VolumeType::New();
+  imgMsg = igtl::ImageMessage::New();
+
+}
+
+int Volumes::ITKtoIGTVolume()
+{
+
+  // Retrieve the image data from ITK image
+  VolumeType::SizeType size;
+  int   sizeIGT[3];
+  int   svsize[3];//   = {256, 256, 1};       // sub-volume size
+  VolumeType::IndexType start;
+  int   svoffset[3];// = {0, 0, 0};           // sub-volume offset
+  int   scalarType;
+  VolumeType::PointType origin;
+  float originIGT[3];
+  //int   numComponents;
+  VolumeType::SpacingType spacing;
+  float spacingIGT[3];// = {0.0854354, 0.0854352, 0.85353};    // spacing (mm/pixel) nog aanpassen
+
+  size = this->volumeData->GetLargestPossibleRegion().GetSize();
+  sizeIGT[0] = size[0]; sizeIGT[1] = size[1]; sizeIGT[2] = size[2];
+  svsize[0] = sizeIGT[0]; svsize[1] = sizeIGT[1]; svsize[2] = sizeIGT[2];
+  start = this->volumeData->GetLargestPossibleRegion().GetIndex();
+  svoffset[0] = start[0]; svoffset[1] = start[1]; svoffset[2] = start[2];
+  spacing = this->volumeData->GetSpacing();
+  spacingIGT[0] = spacing[0]; spacingIGT[1] = spacing[1]; spacingIGT[2] = spacing[2];
+  origin = this->volumeData->GetOrigin();
+  originIGT[0] = origin[0]+((size[0]-1)*spacing[0]/2); originIGT[1] = origin[1]+((size[1]-1)*spacing[1]/2); originIGT[2] = origin[2]+((size[2]-1)*spacing[2]/2);
+  scalarType = igtl::ImageMessage::TYPE_UINT8;
+
+  // Create a new IMAGE type message
+  this->imgMsg->SetDimensions(sizeIGT);
+  this->imgMsg->SetSpacing(spacingIGT);
+  this->imgMsg->SetScalarType(scalarType);
+  this->imgMsg->SetOrigin(originIGT);
+  this->imgMsg->AllocateScalars();
+
+  // Set copy image data into ITK image
+  memcpy(this->imgMsg->GetScalarPointer(), volumeData->GetBufferPointer(), this->imgMsg->GetImageSize());
   // Pack (serialize) and send
   this->imgMsg->Pack();
 
@@ -206,10 +315,8 @@ int ReceiveImage(Clients* client, Images* image, igtl::TimeStamp::Pointer ts, ig
     int c = imgMsg->Unpack(1);
     if (c & igtl::MessageHeader::UNPACK_BODY) // if CRC check is OK
     {
-      image->imgMsg = imgMsg;
       image->IGTtoITKImage();
       std::cerr<<"Image received"<<std::endl;
-      //GTtoITKImage(imgMsg, fixedImage);
       return 0;
     }
   }
@@ -293,7 +400,7 @@ int ReceiveImage(Clients* client, Images* image, igtl::TimeStamp::Pointer ts, ig
   return;
 }*/
 
-int RegisterImage(Images* movingImagep, Images* secondImagep, Images* registeredImagep, RegistrationType::Pointer registration)
+int RegisteredImage(Images* movingImagep, Images* secondImagep, Images* registeredImagep, RegistrationType::Pointer registration)
 {
   // Use resulting transform from the registration to map the moving image into the moving/fixed image space
   typedef itk::ResampleImageFilter< ImageType, ImageType >    ResampleFilterType;
@@ -427,7 +534,7 @@ int RegistrationFunction(Images* fixedImagep, Images* movingImagep, Images* regi
   }
 
   // Create registered version of moving image. NOTE THAT THE SECOND IMAGE IS NOW ALSO THE MOVING IMAGE!!
-  if (0 == RegisterImage(movingImagep, movingImagep, registeredImagep, registration))
+  if (0 == RegisteredImage(movingImagep, fixedImagep, registeredImagep, registration))
   {
     std::cerr << "Registering Image failed" << std::endl;
   }
@@ -443,7 +550,7 @@ int RegistrationFunction(Images* fixedImagep, Images* movingImagep, Images* regi
 
 }
 
-int LoadVolume(char* filename, VolumeType::Pointer volume)
+int LoadVolume(char* filename, Volumes* volume)
 {
 
   typedef itk::ImageFileReader<VolumeType> FileReaderType;
@@ -453,7 +560,7 @@ int LoadVolume(char* filename, VolumeType::Pointer volume)
   try
     {
     reader->Update();
-    volume = reader->GetOutput();
+    volume->volumeData = reader->GetOutput();
     }
   catch (itk::ExceptionObject &ex)
     {
@@ -466,6 +573,60 @@ int LoadVolume(char* filename, VolumeType::Pointer volume)
   std::cerr << "Volume Loaded" << std::endl;
 
   return 1;
+
+}
+
+int resliceImageVolume(Volumes* volume, int start[3], int size[2], Images* sliceImage)
+{
+
+
+  VolumeType::IndexType desiredStart;
+  desiredStart[0]=start[0]; desiredStart[1]=start[1]; desiredStart[2]=start[2];
+  //desiredStart.Fill(1);
+
+  VolumeType::SizeType desiredSize ;
+  desiredSize[0]=size[0]; desiredSize[1]=size[1]; desiredSize[2]=0;
+  //desiredSize.Fill(10);
+  //esiredSize[2]=0;
+
+  VolumeType::RegionType desiredRegion(desiredStart, desiredSize);
+
+  std::cout << "desiredRegion: " << desiredRegion << std::endl;
+
+  typedef itk::ExtractImageFilter< VolumeType, ImageType > FilterType;
+  FilterType::Pointer filter = FilterType::New();
+  filter->SetExtractionRegion(desiredRegion);
+  filter->SetInput(volume->volumeData);
+  #if ITK_VERSION_MAJOR >= 4
+  filter->SetDirectionCollapseToIdentity(); // This is required.
+  #endif
+  filter->Update();
+
+  sliceImage->imageData = filter->GetOutput();
+  //output->DisconnectPipeline();
+  //output->FillBuffer(2);
+
+  VolumeType::SpacingType spacing;       // spacing (mm/pixel)
+  float spacingsliceImage[3];
+  VolumeType::IndexType start1;
+  VolumeType::PointType origin;
+  float startsliceImage[3];
+  float originsliceImage[3];
+  VolumeType::SizeType size1;
+
+  start1 = volume->volumeData->GetLargestPossibleRegion().GetIndex();
+  startsliceImage[0] = start1[0]+start[0]; startsliceImage[1] = start1[1]+start[1]; startsliceImage[2] = start1[2]+start[2];
+  spacing = volume->volumeData->GetSpacing();
+  spacingsliceImage[0] = spacing[0]; spacingsliceImage[1] = spacing[1]; spacingsliceImage[2] = spacing[2];
+  origin = volume->volumeData->GetOrigin();
+  size1 = volume->volumeData->GetLargestPossibleRegion().GetSize();
+  originsliceImage[0] = origin[0]+((size1[0]-1)*spacing[0]/2)+start[0]; originsliceImage[1] = origin[1]+((size1[1]-1)*spacing[1]/2)+start[1]; originsliceImage[2] = origin[2]+((size1[2]-1)*spacing[2]/2)+start[2];
+  //scalarType = igtl::ImageMessage::TYPE_UINT8;
+
+  sliceImage->imageData->SetSpacing(spacingsliceImage);
+  sliceImage->imageData->SetOrigin(originsliceImage);
+  sliceImage->imgMsg->SetOrigin(originsliceImage);
+  sliceImage->imgMsg->SetSpacing(spacingsliceImage);
 
 }
 
@@ -487,47 +648,58 @@ int main(int argc, char* argv[])
 
   // Load volume data
   char* file = argv[1];
-  VolumeType::Pointer volume;
-  LoadVolume(file, volume);
+  Volumes volume;
+  LoadVolume(file, &volume);
 
   // Establish connections
   Clients client1(argv[2],atoi(argv[3]));
   Clients client2(argv[4],atoi(argv[5]));
   Clients client3(argv[6],atoi(argv[7]));
 
+  volume.ITKtoIGTVolume();
+  client2.socket->Send(volume.imgMsg->GetPackPointer(), volume.imgMsg->GetPackSize());
+
   // Create images
   Images fixedImage;
   Images movingImage;
   Images registeredImage;
-  //Images projectionImage;
+  Images sliceImage;
+
+  // Test resliceImage volume
+  int dStart[3]; dStart[0]=0; dStart[1]=0; dStart[2]=0;
+  VolumeType::SizeType size = volume.volumeData->GetLargestPossibleRegion().GetSize();
+  int dSize[2]; dSize[0]=size[0]; dSize[1]=size[1];
+
+  resliceImageVolume(&volume, dStart, dSize, &sliceImage);
+
+  sliceImage.ITKtoIGTImage();
+  sliceImage.imgMsg->SetDeviceName("sliceImage");
+  sliceImage.imgMsg->Pack();
+  client2.socket->Send(sliceImage.imgMsg->GetPackPointer(), sliceImage.imgMsg->GetPackSize());
 
   // Create a message buffer to receive header and image message
-  igtl::MessageHeader::Pointer headerMsg;
-  headerMsg = igtl::MessageHeader::New();
-  igtl::ImageMessage::Pointer imgMsg;
-  imgMsg = igtl::ImageMessage::New();
+  igtl::MessageHeader::Pointer headerMsg = igtl::MessageHeader::New();
 
   // Allocate a time stamp, WHERE IS THIS USED?????????
-  igtl::TimeStamp::Pointer ts;
-  ts = igtl::TimeStamp::New();
+  igtl::TimeStamp::Pointer ts = igtl::TimeStamp::New();
 
   // Receive first image
-  ReceiveImage(&client1, &fixedImage, ts, headerMsg, imgMsg);
+  ReceiveImage(&client1, &fixedImage, ts, headerMsg, fixedImage.imgMsg);
 
   while (1)
   {
     for (int i = 0; i < 100; i ++) //WHY 100????
     {
-      if (ReceiveImage(&client1, &movingImage, ts, headerMsg, imgMsg) == 0)
+      if (ReceiveImage(&client1, &movingImage, ts, headerMsg, movingImage.imgMsg) == 0)
       {
+        if (fixedImage.imgMsg == movingImage.imgMsg){std::cerr<<"zelfde"<<std::endl;}
         // Register fixed and moving image (2D/2D Registration of two subsequential images)
         RegistrationFunction(&fixedImage, &movingImage, &registeredImage);
 
         // Create imageMessage for registered image
         registeredImage.ITKtoIGTImage();
 
-        //TOT HIER
-
+        // Calculate subtraction of registered and fixed image
         /*typedef itk::SubtractImageFilter <ImageType, ImageType > SubtractImageFilterType;
         SubtractImageFilterType::Pointer subtractFilter = SubtractImageFilterType::New ();
         subtractFilter->SetInput1(fixedImage.imageData);
@@ -551,18 +723,23 @@ int main(int argc, char* argv[])
         subtract2.ITKtoIGTImage(temspacing);
         subtract2.imgMsg->SetDeviceName("FixedMinusMoving");
         subtract2.imgMsg->Pack();*/
+
+        // Set massages names
         fixedImage.imgMsg->SetDeviceName("fixed");
         fixedImage.imgMsg->Pack();
         movingImage.imgMsg->SetDeviceName("moving");
         movingImage.imgMsg->Pack();
         registeredImage.imgMsg->SetDeviceName("registered");
         registeredImage.imgMsg->Pack();
-        client2.socket->Send(fixedImage.imgMsg->GetPackPointer(), movingImage.imgMsg->GetPackSize());
+
+        // Send image messages to sliceImager for visualization
+        client3.socket->Send(fixedImage.imgMsg->GetPackPointer(), fixedImage.imgMsg->GetPackSize());
         client3.socket->Send(movingImage.imgMsg->GetPackPointer(), movingImage.imgMsg->GetPackSize());
         //client2.socket->Send(subtract1.imgMsg->GetPackPointer(), subtract1.imgMsg->GetPackSize());
         client3.socket->Send(registeredImage.imgMsg->GetPackPointer(), registeredImage.imgMsg->GetPackSize());
         //client3.socket->Send(subtract2.imgMsg->GetPackPointer(), subtract2.imgMsg->GetPackSize());
 
+        // Set moving image to fixed image so it can be registered to the next image received
         Images temp = movingImage;
         movingImage = fixedImage;
         fixedImage = temp;
